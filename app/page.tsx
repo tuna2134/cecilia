badae02f436b3b9581e28c65653034dc993cfff2
tuna2134@コマリン親衛隊 <hello@tuna2134.dev>
2025-04-28 15:12:41 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,11 @@ export default function Home() {
             datetime: "2025/09",
             description: "Discord botに組み込めたら面白いと思って開発を開始して作った。"
           },
+          {
+            title: "New! ウェブサイト6代目開発開始",
+            datetime: "2025/04",
+            description: "前のサイトを変えたくなった。そして再び一から作ることにした。"
+          },
         ]} />
       </div>
     </>
