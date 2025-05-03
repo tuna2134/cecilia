cc1c04c5d212c194cb374b8b369ce5514b8f7d59
tuna2134 <hello@tuna2134.dev>
2025-05-03 13:54:57 +0000

    lol

--- app/(pages)/blogs/[slug]/page.tsx
+++ app/(pages)/blogs/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import fs from "fs/promises";
+import { Timer, TimeZone } from "iconoir-react";
 
 interface PropsParams {
     slug: string;
@@ -11,7 +12,22 @@ interface Props {
 export default async function Page({ params }: Props) {
     const { slug } = await params;
     const { default: Post, metadata } = await import(`@/blogs/${slug}.mdx`);
-    return <Post />;
+    return (
+        <>
+            <div className="mb-6 rounded-lg border border-gray-300 p-2 shadow-md">
+                <h2 className="border-b-4 border-indigo-600 bg-indigo-600/20 p-1 text-2xl font-bold">
+                    {metadata.title}
+                </h2>
+                <div className="mt-3 flex items-center space-x-2">
+                    <Timer />
+                    <p>作成日時：{metadata.datetime}</p>
+                </div>
+            </div>
+            <div className="prose">
+                <Post />
+            </div>
+        </>
+    );
 }
 
 export async function generateStaticParams() {
