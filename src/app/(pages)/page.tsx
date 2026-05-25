dcf425553537a7cbc924f0adaedec9acf46c4b50
tuna2134 <tuna2134@neody.land>
2026-05-26 08:41:56 +0900

    feat: initialize project with Tailwind CSS, TypeScript, and basic layout
    - Add globals.css to import Tailwind CSS styles
    - Create layout.tsx for the root layout with metadata and font integration
    - Implement Header component with title and subtitle
    - Set up tsconfig.json for TypeScript configuration

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+    return (
+        <p>hello</p>
+    );
+}
+
+export default Home;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header: React.FC = () => {
+    return (
+        <header className="w-full flex flex-col items-center gap-1 py-4 border-b border-gray-400">
+            <small className="text-sm">tuna2134の休憩</small>
+            <h1 className="text-4xl font-bold">tuna2134</h1>
+        </header>
+    )
+}
+
+export default Header;
